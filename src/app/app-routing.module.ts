0136e3d15cad266454f78ba43e4daa5cbe001f7b
Xiocoll <lucas-rod@hotmail.fr>
2020-10-25 13:55:16 +0100

    1 file modified

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,12 +4,20 @@ import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [
   {
-    path: '',
-    loadChildren: './customers/customers.module#CustomersModule'
+    path: 'client',
+    loadChildren: './client/client.module#ClientModule'
   },
   {
-    path: 'orders',
-    loadChildren: './orders/orders.module#OrdersModule'
+    path: 'form',
+    loadChildren: './form/form.module#FormModule'
+  },
+  {
+    path: 'moteur',
+    loadChildren: './moteur/moteur.module#MoteurModule'
+  },
+  {
+    path: 'produit',
+    loadChildren: './produit/produit.module#ProduitModule'
   },
   {
     path: '',

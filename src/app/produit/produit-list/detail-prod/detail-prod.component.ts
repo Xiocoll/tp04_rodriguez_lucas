ff02ee88d0a095cdc45b214d48517b30d384a4f3
Xiocoll <lucas-rod@hotmail.fr>
2020-10-25 23:41:48 +0100

    1 file modified

--- src/app/produit/produit-list/detail-prod/detail-prod.component.ts
+++ src/app/produit/produit-list/detail-prod/detail-prod.component.ts
@@ -1,27 +1,28 @@
-import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
-import { Store } from '@ngxs/store';
-import { Observable } from 'rxjs';
+import { Component, OnInit } from "@angular/core";
+import { ActivatedRoute } from "@angular/router";
+import { Store } from "@ngxs/store";
+import { Observable } from "rxjs";
 import { Produit } from "../../../produit";
 import { ProduitService } from "../../../produit.service";
-import { AddProduit } from '../../../shared/actions/produit-action';
+import { AddProduit } from "../../../shared/actions/produit-action";
 
 @Component({
-  selector: 'app-detail-prod',
-  templateUrl: './detail-prod.component.html',
-  styleUrls: ['./detail-prod.component.css']
+  selector: "app-detail-prod",
+  templateUrl: "./detail-prod.component.html",
+  styleUrls: ["./detail-prod.component.css"]
 })
 export class DetailProdComponent implements OnInit {
-
-  
-  constructor(private appRouter: ActivatedRoute, private produitService: ProduitService, private store: Store) { }
+  constructor(
+    private appRouter: ActivatedRoute,
+    private produitService: ProduitService,
+    private store: Store
+  ) {}
   prod: Observable<Produit>;
+  num: string;
 
   ngOnInit() {
-      this.appRouter.params.subscribe(p => {
-      const num = p["num"];
-      this.prod = this.produitService.getProduitNumero(num);
-    });
+    this.num = this.appRouter.snapshot.params.num;
+    this.prod = this.produitService.getProduitNumero(this.num);
   }
 
   onClickAdd(prod: Produit) {

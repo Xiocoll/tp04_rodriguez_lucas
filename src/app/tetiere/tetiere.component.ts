4e4415b97cef2c14b951efb1563cfdb7e62e415e
Xiocoll <lucas-rod@hotmail.fr>
2020-10-25 23:18:32 +0100

    2 files modified

--- src/app/tetiere/tetiere.component.ts
+++ src/app/tetiere/tetiere.component.ts
@@ -1,20 +1,22 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, OnInit } from '@angular/core';
 
 import { Store } from "@ngxs/store";
-import { Observable } from "rxjs";
-import { ProduitState } from "../shared/states/produit-state";
+import { Observable } from 'rxjs';
+import { ProduitState } from '../shared/states/produit-state';
 
 @Component({
-  selector: "app-tetiere",
-  templateUrl: "./tetiere.component.html",
-  styleUrls: ["./tetiere.component.css"]
+  selector: 'app-tetiere',
+  templateUrl: './tetiere.component.html',
+  styleUrls: ['./tetiere.component.css']
 })
 export class TetiereComponent implements OnInit {
-  constructor(private store: Store) {}
+
+  constructor(private store: Store) { }
 
   nbrProduits: Observable<number> = this.store.select(
-    ProduitState.getNbProduit
-  );
+      ProduitState.getNbProduit);;
 
-  ngOnInit() {}
+  ngOnInit() {
+  }
+
 }
